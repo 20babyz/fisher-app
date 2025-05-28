2e11af5c4336c9f03a081de28ecfcaad223742fc
성영준 <69851018+yxun20@users.noreply.github.com>
2025-05-28 14:35:18 +0900

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -4,25 +4,29 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 import LoginScreen from './screens/LoginScreen';
+import SignupScreen from './screens/SignupScreen';   // ← 추가
+import CheckScreen from './screens/Check';           // ← 추가
 
 export type RootStackParamList = {
   Login: undefined;
-
+  Signup: undefined;
+  Check: undefined;
 };
 
 const Stack = createNativeStackNavigator<RootStackParamList>();
 
-function App(): React.ReactElement {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator
-        initialRouteName="Login"
-        screenOptions={{ headerShown: false }}
-      >
-        <Stack.Screen name="Login" component={LoginScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
-}
+const App: React.FC = () => (
+  <NavigationContainer>
+    <Stack.Navigator
+      initialRouteName="Login"
+      screenOptions={{ headerShown: false }}
+    >
+      <Stack.Screen name="Login" component={LoginScreen} />
+      <Stack.Screen name="Signup" component={SignupScreen} />
+      <Stack.Screen name="Check" component={CheckScreen} />
+
+    </Stack.Navigator>
+  </NavigationContainer>
+);
 
 export default App;
